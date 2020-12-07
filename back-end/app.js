ec4a281a4f3c3eb537bf8a425ae133154350629a
luispereira1999 <67525203+luispereira1999@users.noreply.github.com>
2020-12-07 20:04:07 +0000

    Criar rotas para registo e login

--- back-end/app.js
+++ back-end/app.js
@@ -16,3 +16,26 @@ app.get("/", (req, res) => {
 // necessário para pré-processar o corpo das solicitações POST
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+
+// rotas da api
+// const adminRoute = require("./routes/admin-route")
+// const clientRoute = require("./routes/client-route")
+// const deliveryRoute = require("./routes/delivery-route")
+// const driverRoute = require("./routes/driver-route")
+// const merchantRoute = require("./routes/merchant-route")
+// const orderRoute = require("./routes/order-route")
+// const productRoute = require("./routes/product-route")
+// const userRoute = require("./routes/user-route")
+const signupRoute = require("./routes/signup-route")
+const loginRoute = require("./routes/login-route")
+
+// app.use("/api/admins", adminRoute)
+// app.use("/api/clients", clientRoute)
+// app.use("/api/deliveries", deliveryRoute)
+// app.use("/api/drivers", driverRoute)
+// app.use("/api/merchants", merchantRoute)
+// app.use("/api/orders", orderRoute)
+// app.use("/api/products", productRoute)
+// app.use("/api/users", userRoute)
+app.use("/api/login", loginRoute)
+app.use("/api/signup", signupRoute)
