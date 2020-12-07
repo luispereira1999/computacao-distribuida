bcca3959822a4b62a29f071b71811d90619b1e51
luispereira1999 <67525203+luispereira1999@users.noreply.github.com>
2020-12-07 20:08:42 +0000

    Criar registo empresa

--- back-end/controllers/signup-controller.js
+++ back-end/controllers/signup-controller.js
@@ -2,6 +2,7 @@ const database = require("../utils/database")
 const md5 = require("md5")
 var User = require("../models/user")
 var Client = require("../models/client")
+var Merchant = require("../models/merchant")
 
 
 module.exports = {
@@ -23,6 +24,29 @@ module.exports = {
          db.close()
       })
    },
+
+
+   signupMerchant: async(req, res) => {
+      const db = database.connect()
+
+      var user = getUser(req.body)
+      var sql = "INSERT INTO Users (username, password, name, email, birth_date, gender, phone_number, city, address, zip_code, nif, active) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
+      var params = [user.username, md5(user.password), user.name, user.email, user.birth_date, user.gender, user.phone_number, user.city, user.address, user.zip_code, user.nif, 0]
+
+      // inserir na tabela utilizadores
+      db.run(sql, params, function(err) {
+         if (err) {
+            return res.status(400).json({ "error": err.message })
+         }
+
+         var data = {
+            user_id: this.lastID,
+            registration_request: req.body.registration_request
+         }
+         insertInMerchantsTable(db, res, data)
+         db.close()
+      })
+   }
 }
 
 
@@ -33,6 +57,11 @@ function getUser(data) {
 
 function getClient(data) {
    return new Client(data)
+}
+
+
+function getMerchant(data) {
+   return new Merchant(data)
 }
 
 
@@ -47,6 +76,22 @@ function insertInClientsTable(db, res, data) {
       }
       res.json({
          "message": "Cliente registado com sucesso!"
+      })
+   })
+}
+
+
+function insertInMerchantsTable(db, res, data) {
+   var merchant = getMerchant(data)
+   var sql = "INSERT INTO Merchants (user_id, registration_request) VALUES (?, ?)"
+   var params = [merchant.user_id, merchant.registration_request]
+
+   db.run(sql, params, function(err) {
+      if (err) {
+         return res.status(400).json({ "error": err.message })
+      }
+      res.json({
+         "message": "Empresa registada com sucesso!"
       })
    })
 }
