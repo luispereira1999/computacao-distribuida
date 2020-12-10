f73e3ce0b432fde0e427ea3d1cf879ab25c94c13
luispereira1999 <67525203+luispereira1999@users.noreply.github.com>
2020-12-10 12:00:37 +0000

    Verificar se nome de utilizador e email já existem ao fazer registo

--- back-end/controllers/signup-controller.js
+++ back-end/controllers/signup-controller.js
@@ -8,46 +8,62 @@ var Admin = require("../models/admin")
 
 
 module.exports = {
-   signupClient: async(req, res) => {
-      const db = database.connect()
-      if (checkFields(req, res, "client"))
-         return
+   signupClient: async (req, res) => {
+      const db = database.connect();
+
+      var errors = await checkFields(req, "client");
+      if (errors.exist) {
+         return res.status(400).json({ "error": errors.message.join(" | ") });
+      }
+
+      var userExist = await checkUsernameOrEmailAlreadyExist(db, req, res);
+      if (userExist.exist) {
+         return res.status(400).json({ "error": userExist.message });
+      }
 
       var user = getUser(req.body)
       var sql = "INSERT INTO Users (username, password, name, email, birth_date, gender, phone_number, city, address, zip_code, nif, active) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
       var params = [user.username, md5(user.password), user.name, user.email, user.birth_date, user.gender, user.phone_number, user.city, user.address, user.zip_code, user.nif, 1]
 
       // inserir na tabela utilizadores
-      db.run(sql, params, function(err) {
+      db.run(sql, params, function (err) {
          if (err) {
-            return res.status(400).json({ "error": err.message })
+            return res.status(500).json({ "error": err.message })
          }
 
-         var data = { user_id: this.lastID }
+         var data = { "user_id": this.lastID }
          insertInClientsTable(db, res, data)
          db.close()
       })
    },
 
 
-   signupMerchant: async(req, res) => {
-      const db = database.connect()
-      if (checkFields(req, res, "merchant"))
-         return
+   signupMerchant: async (req, res) => {
+      const db = database.connect();
+
+      var errors = await checkFields(req, "client");
+      if (errors.exist) {
+         return res.status(400).json({ "error": errors.message.join(" | ") });
+      }
+
+      var userExist = await checkUsernameOrEmailAlreadyExist(db, req, res);
+      if (userExist.exist) {
+         return res.status(400).json({ "error": userExist.message });
+      }
 
       var user = getUser(req.body)
       var sql = "INSERT INTO Users (username, password, name, email, birth_date, gender, phone_number, city, address, zip_code, nif, active) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
       var params = [user.username, md5(user.password), user.name, user.email, user.birth_date, user.gender, user.phone_number, user.city, user.address, user.zip_code, user.nif, 0]
 
       // inserir na tabela utilizadores
-      db.run(sql, params, function(err) {
+      db.run(sql, params, function (err) {
          if (err) {
-            return res.status(400).json({ "error": err.message })
+            return res.status(500).json({ "error": err.message })
          }
 
          var data = {
-            user_id: this.lastID,
-            registration_request: req.body.registration_request
+            "user_id": this.lastID,
+            "registration_request": req.body.registration_request
          }
          insertInMerchantsTable(db, res, data)
          db.close()
@@ -55,24 +71,32 @@ module.exports = {
    },
 
 
-   signupDriver: async(req, res) => {
-      const db = database.connect()
-      if (checkFields(req, res, "driver"))
-         return
+   signupDriver: async (req, res) => {
+      const db = database.connect();
+
+      var errors = await checkFields(req, "client");
+      if (errors.exist) {
+         return res.status(400).json({ "error": errors.message.join(" | ") });
+      }
+
+      var userExist = await checkUsernameOrEmailAlreadyExist(db, req, res);
+      if (userExist.exist) {
+         return res.status(400).json({ "error": userExist.message });
+      }
 
       var user = getUser(req.body)
       var sql = "INSERT INTO Users (username, password, name, email, birth_date, gender, phone_number, city, address, zip_code, nif, active) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
       var params = [user.username, md5(user.password), user.name, user.email, user.birth_date, user.gender, user.phone_number, user.city, user.address, user.zip_code, user.nif, 0]
 
       // inserir na tabela utilizadores
-      db.run(sql, params, function(err) {
+      db.run(sql, params, function (err) {
          if (err) {
-            return res.status(400).json({ "error": err.message })
+            return res.status(500).json({ "error": err.message })
          }
 
          var data = {
-            user_id: this.lastID,
-            registration_request: req.body.registration_request
+            "user_id": this.lastID,
+            "registration_request": req.body.registration_request
          }
          insertInDriversTable(db, res, data)
          db.close()
@@ -80,24 +104,32 @@ module.exports = {
    },
 
 
-   signupAdmin: async(req, res) => {
-      const db = database.connect()
-      if (checkFields(req, res, "admin"))
-         return
+   signupAdmin: async (req, res) => {
+      const db = database.connect();
+
+      var errors = await checkFields(req, "client");
+      if (errors.exist) {
+         return res.status(400).json({ "error": errors.message.join(" | ") });
+      }
+
+      var userExist = await checkUsernameOrEmailAlreadyExist(db, req, res);
+      if (userExist.exist) {
+         return res.status(400).json({ "error": userExist.message });
+      }
 
       var user = getUser(req.body)
       var sql = "INSERT INTO Users (username, password, name, email, birth_date, gender, phone_number, city, address, zip_code, nif, active) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
       var params = [user.username, md5(user.password), user.name, user.email, user.birth_date, user.gender, user.phone_number, user.city, user.address, user.zip_code, user.nif, 0]
 
       // inserir na tabela utilizadores
-      db.run(sql, params, function(err) {
+      db.run(sql, params, function (err) {
          if (err) {
-            return res.status(400).json({ "error": err.message })
+            return res.status(500).json({ "error": err.message })
          }
 
          var data = {
-            user_id: this.lastID,
-            registration_request: req.body.registration_request
+            "user_id": this.lastID,
+            "registration_request": req.body.registration_request
          }
          insertInAdminsTable(db, res, data)
          db.close()
@@ -106,7 +138,7 @@ module.exports = {
 }
 
 
-function checkFields(req, res, profile) {
+function checkFields(req, profile) {
    var errors = []
 
    if (profile == "client") {
@@ -123,8 +155,10 @@ function checkFields(req, res, profile) {
          errors.push("O email não foi preenchido.")
       }
       if (errors.length) {
-         res.status(400).json({ "error": errors.join(" | ") })
-         return true
+         return ({
+            "exist": true,
+            "message": errors
+         })
       }
    } else if (profile == "merchant" || profile == "driver" || profile == "admin") {
       if (!req.body.username) {
@@ -143,10 +177,48 @@ function checkFields(req, res, profile) {
          errors.push("O pedido de registo não foi preenchido.")
       }
       if (errors.length) {
-         res.status(400).json({ "error": errors.join(" | ") })
-         return true
+         return ({
+            "exist": true,
+            "message": errors
+         })
       }
    }
+
+   return ({ "exist": false })
+}
+
+
+function checkUsernameOrEmailAlreadyExist(db, req, res) {
+   return new Promise((resolve) => {
+      var user = getUser(req.body)
+      var sql = "SELECT * FROM users WHERE username = ? OR email = ? LIMIT 1"
+      var params = [user.username, user.email]
+      var userExist = ({
+         "exist": false,
+         "message": "nao existe"
+      })
+
+      db.each(sql, params, (err, row) => {
+         if (err) {
+            return userExist = {
+               "exist": false,
+               "message": err.message
+            }
+         }
+
+         if (row) {
+            return userExist = {
+               "exist": true,
+               "message": "Nome de utilizador ou email já existem. Coloque outro."
+            }
+         }
+         else {
+            return userExist = { "exist": false }
+         }
+      }, () => {
+         resolve(userExist)
+      })
+   })
 }
 
 
@@ -180,11 +252,11 @@ function insertInClientsTable(db, res, data) {
    var sql = "INSERT INTO Clients (user_id) VALUES (?)"
    var params = client.user_id
 
-   db.run(sql, params, function(err) {
+   db.run(sql, params, function (err) {
       if (err) {
-         return res.status(400).json({ "error": err.message })
+         return res.status(500).json({ "error": err.message })
       }
-      res.json({
+      return res.json({
          "message": "Cliente registado com sucesso!"
       })
    })
@@ -196,11 +268,11 @@ function insertInMerchantsTable(db, res, data) {
    var sql = "INSERT INTO Merchants (user_id, registration_request) VALUES (?, ?)"
    var params = [merchant.user_id, merchant.registration_request]
 
-   db.run(sql, params, function(err) {
+   db.run(sql, params, function (err) {
       if (err) {
-         return res.status(400).json({ "error": err.message })
+         return res.status(500).json({ "error": err.message })
       }
-      res.json({
+      return res.json({
          "message": "Empresa registada com sucesso!"
       })
    })
@@ -212,11 +284,11 @@ function insertInDriversTable(db, res, data) {
    var sql = "INSERT INTO Drivers (user_id, registration_request) VALUES (?, ?)"
    var params = [driver.user_id, driver.registration_request]
 
-   db.run(sql, params, function(err) {
+   db.run(sql, params, function (err) {
       if (err) {
-         return res.status(400).json({ "error": err.message })
+         return res.status(500).json({ "error": err.message })
       }
-      res.json({
+      return res.json({
          "message": "Condutor registado com sucesso!"
       })
    })
@@ -228,13 +300,12 @@ function insertInAdminsTable(db, res, data) {
    var sql = "INSERT INTO Admins (user_id, registration_request) VALUES (?, ?)"
    var params = [admin.user_id, admin.registration_request]
 
-   db.run(sql, params, function(err) {
+   db.run(sql, params, function (err) {
       if (err) {
-         return res.status(400).json({ "error": err.message })
+         return res.status(500).json({ "error": err.message })
       }
-      res.json({
+      return res.json({
          "message": "Admin registado com sucesso!"
       })
    })
-
 }
