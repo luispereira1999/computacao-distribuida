f49e6d0f756f64a640e1815d564c8fd5e851fd0a
luispereira1999 <67525203+luispereira1999@users.noreply.github.com>
2020-12-12 02:29:51 +0000

    Criar sessão quando o utilizador faz login

--- back-end/controllers/login-controller.js
+++ back-end/controllers/login-controller.js
@@ -1,52 +1,66 @@
-const database = require("../utils/database")
-const md5 = require("md5")
-var User = require("../models/user")
+const database = require("../utils/database");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+var User = require("../models/user");
 
 
 module.exports = {
-   login: async(req, res) => {
-      const db = database.connect()
-      if (checkFields(req, res))
-         return
+   login: async (req, res) => {
+      const db = database.connect();
 
-      var user = new User(req.body)
-      var sql = "SELECT * FROM users WHERE username = ? AND password = ?"
-      var params = [user.username, md5(user.password)]
+      if (errors = checkFields(req, res))
+         return res.status(400).json({ "error": errors.join(" | ") });
+
+      var user = new User(req.body);
+      var sql = "SELECT * FROM users WHERE username = ?";
+      var params = user.username;
 
       // obter utilizador
-      db.get(sql, params, function(err, row) {
+      db.get(sql, params, async function (err, row) {
          if (err) {
-            return res.status(400).json({ "error": err.message })
+            return res.status(400).json({ "error": err.message });
          }
 
          if (row) {
-            res.json({
-               "message": "O utilizador: " + row.username + " efetuou login com sucesso!",
-               "data": row
-            })
-         } else {
-            res.json({
-               "message": "Nome de utilizador ou senha inválidos. Tente novamente!"
-            })
+            const checkPassword = await bcrypt.compareSync(user.password, row.password);
+
+            if (checkPassword) {
+               // dados da sessão
+               const token = jwt.sign({
+                  id_user: row.id_user,
+                  username: row.username,
+                  nome: row.nome,
+                  email: row.email
+               }, "hard-secret", { expiresIn: "24h" });
+
+               res.json({
+                  "message": "O utilizador: " + row.username + " efetuou login com sucesso!",
+                  "session": token,
+                  "data": row
+               });
+            } else {
+               res.json({
+                  "message": "Nome de utilizador ou senha inválidos. Tente novamente!"
+               });
+            }
          }
 
-         db.close()
-      })
+         db.close();
+      });
    }
-}
+};
 
 
 function checkFields(req, res) {
-   var errors = []
+   var errors = [];
 
    if (!req.body.username) {
-      errors.push("O nome de utilizador não foi preenchido.")
+      errors.push("O nome de utilizador não foi preenchido.");
    }
    if (!req.body.password) {
-      errors.push("A senha não foi preenchida.")
+      errors.push("A senha não foi preenchida.");
    }
    if (errors.length) {
-      res.status(400).json({ "error": errors.join(" | ") })
-      return true
+      return errors;
    }
 }
