dc505e0e8328ac59bac89f2966879ca9a5bbea13
luispereira1999 <67525203+luispereira1999@users.noreply.github.com>
2020-12-13 22:28:52 +0000

    Ver informações do utilizador

--- back-end/controllers/user-controller.js
+++ back-end/controllers/user-controller.js
@@ -4,6 +4,55 @@ var User = require("../models/user");
 
 
 module.exports = {
+   view: async (req, res, next) => {
+      const db = database.connect();
+
+      // obter dados da resposta
+      const decodedSession = jwt.verify(req.body.session, "hard-secret");
+      var user = new User(decodedSession);
+
+      if (checkValidID(req.params.id, decodedSession.id))
+         return res.status(400).json({ "error": "Ups! ID do utilizador não corresponde ao da sessão atual." });
+
+      // selecionar utilizador na base de dados
+      var sql = "SELECT * FROM Users WHERE id = ?";
+      var params = user.id;
+      db.get(sql, params, function (err, row) {
+         if (err)
+            return res.status(400).json({ "error": res.message });
+
+         return res.json({ "data": row });
+      });
+
+      db.close();
+   },
+
+
+   // editar utilizador
+   // edit: async (req, res, next) => {
+   //    const db = database.connect();
+
+   //    // obter dados da resposta
+   //    const decodedSession = jwt.verify(req.body.session, "hard-secret");
+   //    var user = new User(decodedSession);
+
+   //    if (checkValidID(req.params.id, decodedSession.id))
+   //       return res.status(400).json({ "error": "Ups! ID do utilizador não corresponde ao da sessão atual." });
+
+   //    // editar na tabela utilizadores
+   //    var sql = "UPDATE Users SET username = ?, password = ?, name = ?, email = ?, birth_date = ?, gender = ?, phone_number = ?, city = ?, address = ?, zip_code = ?, nif = ? WHERE id = ?";
+   //    var params = [user.username, hash, user.name, user.email, user.birth_date, user.gender, user.phone_number, user.city, user.address, user.zip_code, user.nif];
+   //    db.run(sql, params, function (err) {
+   //       if (err)
+   //          return res.status(400).json({ "error": res.message });
+
+   //       return res.json({ "message": "Utilizador editado com sucesso!" });
+   //    });
+
+   //    db.close();
+   // },
+
+
    // excluir utilizador
    delete: async (req, res, next) => {
       const db = database.connect();
@@ -13,7 +62,7 @@ module.exports = {
       var user = new User(decodedSession);
 
       if (checkValidID(req.params.id, decodedSession.id))
-         return res.status(400).json({ "error": "Ups! ID do utilizador a ser excluído não corresponde ao da sessão atual." });
+         return res.status(400).json({ "error": "Ups! ID do utilizador não corresponde ao da sessão atual." });
 
       // apagar na tabela utilizadores
       var sql = "DELETE FROM Users WHERE id = ?";
--- back-end/routes/user-route.js
+++ back-end/routes/user-route.js
@@ -3,9 +3,10 @@ const router = express.Router();
 const userController = require("../controllers/user-controller");
 const validateLogin = require("../middlewares/authenticate");
 
-// editar utilizador
-// bloquear conta
-// terminar sessão
+router.get("/view/:id", validateLogin, userController.viewInfo);
+// router.patch("/edit/:id", validateLogin, userController.edit);
+// router.patch("/edit/:id", validateLogin, userController.block);
+// router.post("/edit/:id", validateLogin, userController.logout);
 router.delete("/delete/:id", validateLogin, userController.delete);
 
 module.exports = router;
