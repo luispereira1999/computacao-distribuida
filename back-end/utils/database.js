d0c407c6d9d41284e5963da03b644ed1f58cc0d3
luispereira1999 <67525203+luispereira1999@users.noreply.github.com>
2020-12-14 09:36:17 +0000

    Adicionar tipo de utilizador à base de dados

--- back-end/utils/database.js
+++ back-end/utils/database.js
@@ -1,12 +1,12 @@
-const sqlite3 = require("sqlite3")
-const dbSource = "./utils/database.db"
+const sqlite3 = require("sqlite3");
+const dbSource = "./utils/database.db";
 
 module.exports = {
    connect: () => {
-      return new sqlite3.Database(dbSource, sqlite3.OPEN_READWRITE, (err) => {
+      return new sqlite3.Database(dbSource, (err) => {
          if (err) {
-            return console.log("Não foi possível conectar à base de dados:", err.message)
+            return console.log("Não foi possível conectar à base de dados:", err.message);
          }
       })
-   },
-}
+   }
+};
