27867274e74eebdb0d83badec87e4c96e8724933
luispereira1999 <67525203+luispereira1999@users.noreply.github.com>
2020-12-14 09:34:44 +0000

    Ativar utilizador

--- back-end/controllers/user-controller.js
+++ back-end/controllers/user-controller.js
@@ -7,7 +7,7 @@ module.exports = {
    view: async (req, res, next) => {
       const db = database.connect();
 
-      // obter dados da resposta
+      // obter dados da request
       const decodedSession = jwt.verify(req.body.session, "hard-secret");
       var user = new User(decodedSession);
 
@@ -29,35 +29,60 @@ module.exports = {
 
 
    // editar utilizador
-   // edit: async (req, res, next) => {
-   //    const db = database.connect();
+   edit: async (req, res, next) => {
+      const db = database.connect();
 
-   //    // obter dados da resposta
-   //    const decodedSession = jwt.verify(req.body.session, "hard-secret");
-   //    var user = new User(decodedSession);
+      // obter dados da request
+      const decodedSession = jwt.verify(req.body.session, "hard-secret");
+      var user = new User(decodedSession);
 
-   //    if (checkValidID(req.params.id, decodedSession.id))
-   //       return res.status(400).json({ "error": "Ups! ID do utilizador não corresponde ao da sessão atual." });
+      if (checkValidID(req.params.id, decodedSession.id))
+         return res.status(400).json({ "error": "Ups! ID do utilizador não corresponde ao da sessão atual." });
 
-   //    // editar na tabela utilizadores
-   //    var sql = "UPDATE Users SET username = ?, password = ?, name = ?, email = ?, birth_date = ?, gender = ?, phone_number = ?, city = ?, address = ?, zip_code = ?, nif = ? WHERE id = ?";
-   //    var params = [user.username, hash, user.name, user.email, user.birth_date, user.gender, user.phone_number, user.city, user.address, user.zip_code, user.nif];
-   //    db.run(sql, params, function (err) {
-   //       if (err)
-   //          return res.status(400).json({ "error": res.message });
+      // editar na tabela utilizadores
+      var sql = "UPDATE Users SET username = ?, password = ?, name = ?, email = ?, birth_date = ?, gender = ?, phone_number = ?, city = ?, address = ?, zip_code = ?, nif = ? WHERE id = ?";
+      var params = [user.username, hash, user.name, user.email, user.birth_date, user.gender, user.phone_number, user.city, user.address, user.zip_code, user.nif];
+      db.run(sql, params, function (err) {
+         if (err)
+            return res.status(400).json({ "error": res.message });
 
-   //       return res.json({ "message": "Utilizador editado com sucesso!" });
-   //    });
+         return res.json({ "message": "Utilizador editado com sucesso!" });
+      });
 
-   //    db.close();
-   // },
+      db.close();
+   },
+
+
+   // ativar utilizador
+   active: async (req, res, next) => {
+      const db = database.connect();
+
+      // obter dados da request
+      const decodedSession = jwt.verify(req.body.session, "hard-secret");
+      var user = new User(decodedSession);
+
+      if (checkValidID(req.params.id, decodedSession.id))
+         return res.status(400).json({ "error": "Ups! ID do utilizador não corresponde ao da sessão atual." });
+
+      // ativar utilizador na base de dados
+      var sql = "UPDATE Users SET active = 1 WHERE id = ?";
+      var params = user.id;
+      db.run(sql, params, function (err) {
+         if (err)
+            return res.status(400).json({ "error": res.message });
+
+         return res.json({ "message": "Utilizador ativado com sucesso!" });
+      });
+
+      db.close();
+   },
 
 
    // excluir utilizador
    delete: async (req, res, next) => {
       const db = database.connect();
 
-      // obter dados da resposta
+      // obter dados da request
       const decodedSession = jwt.verify(req.body.session, "hard-secret");
       var user = new User(decodedSession);
 
--- back-end/routes/user-route.js
+++ back-end/routes/user-route.js
@@ -3,10 +3,11 @@ const router = express.Router();
 const userController = require("../controllers/user-controller");
 const validateLogin = require("../middlewares/authenticate");
 
-router.get("/view/:id", validateLogin, userController.viewInfo);
+router.get("/view/:id", validateLogin, userController.view);
 // router.patch("/edit/:id", validateLogin, userController.edit);
-// router.patch("/edit/:id", validateLogin, userController.block);
-// router.post("/edit/:id", validateLogin, userController.logout);
+// router.patch("/block/:id", validateLogin, userController.block);
+router.patch("/active/:id", validateLogin, userController.active);
+// router.post("/logout/:id", validateLogin, userController.logout);
 router.delete("/delete/:id", validateLogin, userController.delete);
 
 module.exports = router;
