04372279f695cda95a3ae586745715652d438a8a
luispereira1999 <67525203+luispereira1999@users.noreply.github.com>
2020-12-31 03:28:26 +0000

    Corrigir erros nas entregas

--- back-end/controllers/deliveries-controller.js
+++ back-end/controllers/deliveries-controller.js
@@ -1,22 +1,25 @@
 const database = require("../utils/database");
 var Order = require("../models/order");
 var Delivery = require("../models/delivery");
-var User = require("../models/user");
 
 
 module.exports = {
    accept: async (req, res) => {
       const db = database.connect();
 
-      var orderExist = await checkOrderExist(db, req);
+      var errors = await checkFields(req, 1);
+      if (errors.exist)
+         return res.status(400).json({ "error": errors.message.join(" | ") });
+
+      var orderExist = await checkOrderExist(db, req.body.order_id);
       if (!orderExist.exist)
          return res.status(400).json({ "error": orderExist.message });
 
-      var allData = Object.assign({ "order_id": req.body.id, "user_id": req.user.id });
+      var allData = Object.assign({ "order_id": req.body.order_id, "user_id": req.user.id });
       var delivery = new Delivery(allData);
 
       // inserir na tabela encomendas
-      var sql = "INSERT INTO Deliveries (pending, completed, order_id, user_id) VALUES (1, 0, ?, ?)";
+      var sql = "INSERT INTO Deliveries (order_id, user_id, pending, completed) VALUES (?, ?, 1, 0)";
       var params = [delivery.order_id, delivery.user_id];
       db.run(sql, params, function (err) {
          if (err)
@@ -32,20 +35,20 @@ module.exports = {
    complete: async (req, res) => {
       const db = database.connect();
 
-      var order = new Order(req.params);
-      var user = new User(req.user);
+      var allData = Object.assign({ "order_id": req.body.order_id, "user_id": req.user.id });
+      var delivery = new Delivery(allData);
 
-      // atualizar produto na base de dados
-      var sql = "UPDATE Orders SET canceled = 1 WHERE id = ? AND client_id = ? AND accepted = 0";
-      var params = [order.id, user.id];
-      db.run(sql, params, function (err, row) {
+      // atualizar entrega na base de dados
+      var sql = "UPDATE Deliveries SET pending = 0, completed = 1 WHERE order_id = ? AND user_id = ? AND pending = 1 AND completed = 0";
+      var params = [delivery.order_id, delivery.user_id];
+      db.run(sql, params, function (err) {
          if (err)
             return res.status(500).json({ "error": err.message });
 
          if (this.changes == 0)
-            return res.status(400).json({ "message": "Oh! A encomenda não existe ou já foi aceite pelo condutor." });
+            return res.status(400).json({ "message": "Oh! A entrega não existe ou já foi entregue pelo condutor." });
 
-         res.status(200).json({ "message": "Encomenda excluída com sucesso!" });
+         res.status(200).json({ "message": "Entrega concluída com sucesso!" });
       });
 
       db.close();
@@ -53,19 +56,28 @@ module.exports = {
 };
 
 
-function checkOrderExist(db, req) {
+function checkFields(req) {
+   var errors = [];
+
+   if (!req.body.order_id)
+      errors.push("O ID da encomenda não foi preenchido.");
+
+   if (errors.length)
+      return ({ "exist": true, "message": errors });
+   else return ({ "exist": false });
+}
+
+
+function checkOrderExist(db, orderId) {
    return new Promise((resolve) => {
-      var order = new Order(req.body);
-      var sql = "SELECT id FROM Orders WHERE id = ? LIMIT 1";
+      var order = new Order({ "id": orderId });
+      var sql = "SELECT accepted FROM Orders WHERE id = ? AND canceled = 0";
       var params = order.id;
-      var orderExist = { "exist": false, "message": "Oh! A encomenda não existe." };
+      var orderExist = { "exist": false, "message": "Oh! A encomenda não existe ou foi cancelada." };
 
       db.each(sql, params, (err) => {
          if (err)
-            return orderExist = {
-               "exist": false,
-               "message": err.message
-            };
+            return orderExist = { "exist": false, "message": err.message };
 
          return orderExist = { "exist": true };
       }, () => {
--- back-end/models/delivery.js
+++ back-end/models/delivery.js
@@ -1,8 +1,8 @@
 module.exports = class {
    constructor(data) {
-      this.id = data.id ? data.id : null;
-      this.pending = data.pending ? data.pending : null;
       this.order_id = data.order_id ? data.order_id : null;
+      this.pending = data.pending ? data.pending : null;
+      this.complete = data.complete ? data.complete : null;
       this.user_id = data.user_id ? data.user_id : null;
    }
 }
