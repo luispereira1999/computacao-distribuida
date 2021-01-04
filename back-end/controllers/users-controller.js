ef065094af8c7e3193c61d0eb7d478d3847a01ee
luispereira1999 <67525203+luispereira1999@users.noreply.github.com>
2021-01-04 22:05:15 +0000

    Editar foto do utilizador

--- back-end/controllers/users-controller.js
+++ back-end/controllers/users-controller.js
@@ -42,6 +42,29 @@ module.exports = {
    // },
 
 
+   editPhoto: async (req, res, next) => {
+      const db = database.connect();
+
+      var errors = await checkInvalidFields(req, "edit-photo");
+      if (errors.exist)
+         return res.status(400).json({ "message": errors.message.join(" | ") });
+
+      var user = new User({ "id": req.user.id, "url_photo": req.file.filename });
+
+      // atualizar utilizador na base de dados
+      var sql = "UPDATE Users SET url_photo = ? WHERE id = ?";
+      var params = [user.url_photo, user.id];
+      db.run(sql, params, function (err) {
+         if (err)
+            return res.status(500).json({ "message": "Oh! " + err.message });
+
+         res.status(200).json({ "message": "Foto do utilizador editada com sucesso!" });
+      });
+
+      db.close();
+   },
+
+
    getByNotAccepted: async (req, res, next) => {
       const db = database.connect();
 
@@ -165,5 +188,65 @@ module.exports = {
       });
 
       db.close();
+   }
+}
+
+
+function checkInvalidFields(req, operation) {
+   var errors = [];
+
+   switch (operation) {
+      case "edit-data":
+         if (!req.body.name)
+            errors.push("Ups! O nome do produto não foi preenchido.");
+         if (!req.body.stock)
+            errors.push("O stock não foi preenchida.");
+         if (!req.body.price)
+            errors.push("O preço não foi preenchido.");
+         if (!req.body.description)
+            errors.push("A descrição não foi preenchida.");
+         if (!req.file)
+            errors.push("A foto do produto não foi preenchida.");
+         else {
+            if (req.file.mimetype != "image/png" && req.file.mimetype != "image/jpg" && req.file.mimetype != "image/jpeg") {
+               removeFile(req.file.path);
+               errors.push("O foto do produto foi inserida incorretamente.");
+            }
+         }
+
+         if (errors.length)
+            return { "exist": true, "message": errors };
+         else
+            return { "exist": false };
+      case "edit-photo":
+         if (!req.file)
+            errors.push("A foto do utilizador não foi preenchida.");
+         else {
+            if (req.file.mimetype != "image/png" && req.file.mimetype != "image/jpg" && req.file.mimetype != "image/jpeg") {
+               removeFile(req.file.path);
+               errors.push("O foto do utilizador foi inserida incorretamente.");
+            }
+         }
+
+         if (errors.length)
+            return { "exist": true, "message": errors };
+         else
+            return { "exist": false };
+      case "edit-driving-license":
+         if (!req.file)
+            errors.push("A foto do produto não foi preenchida.");
+         else {
+            if (req.file.mimetype != "image/png" && req.file.mimetype != "image/jpg" && req.file.mimetype != "image/jpeg") {
+               removeFile(req.file.path);
+               errors.push("O foto do produto foi inserida incorretamente.");
+            }
+         }
+
+         if (errors.length)
+            return { "exist": true, "message": errors };
+         else
+            return { "exist": false };
+      default:
+         return { "exist": false };
    }
 }
