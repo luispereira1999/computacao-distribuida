5cde3cfcae56714949d73e5fb389666952279bdd
luispereira1999 <67525203+luispereira1999@users.noreply.github.com>
2021-01-07 14:24:15 +0000

    Organizar projeto para fazer deploy

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Picand Go
-Aplicação web de venda e entrega de bens!
+Aplicação web de venda e entrega de bens.
 
 
 <img width="250" height="150" src="https://github.com/luispereira1999/picand-go/blob/main/Logotipo/Logotipo.png">
---
+++ index.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+var path = require('path');
+
+// iniciar servidor
+var app = express();
+
+// necessário para pré-processar o corpo das solicitações POST
+app.use(bodyParser.urlencoded({extended: false})); 
+app.use(bodyParser.json()); 
+app.use(express.static(path.join(__dirname, 'public')));
+
+// rotas
+var    defaultRoute = require("./routes/default-route");
+var registerRoute = require("./routes/register-route");
+var loginRoute = require("./routes/login-route");
+var usersRoute = require("./routes/users-route");
+var productsRoute = require("./routes/products-route");
+var ordersRoute = require("./routes/orders-route");
+var deliveriesRoute = require("./routes/deliveries-route");
+
+app.use(morgan('dev'));
+
+app.use("/api", defaultRoute);
+app.use("/api/login", loginRoute);
+app.use("/api/register", registerRoute);
+app.use("/api/users", usersRoute);
+app.use("/api/products", productsRoute);
+app.use("/api/orders", ordersRoute);
+app.use("/api/deliveries", deliveriesRoute);
+
+// resolver problemas de acesso negado - CORS
+app.use(function (req, res, next) {
+   res.setHeader('Access-Control-Allow-Origin', '*');
+   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,Authorization');
+   res.setHeader('Access-Control-Allow-Credentials', true);
+   res.setHeader('Access-Control-Allow-Authorization', 'Bearer *');
+   next();
+});
+
+// rota quando uma rota não existe
+app.use(function (req, res, next) {
+   res.status(404).json({ "error": "Oh! A página não foi encontrada." });
+});
+
+module.exports = app;
