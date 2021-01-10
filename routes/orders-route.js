efba6226bbfb7b2094e1a1c1b4d734e50154a150
luispereira1999 <67525203+luispereira1999@users.noreply.github.com>
2021-01-10 16:39:05 +0000

    Obter encomendas da empresa

--- routes/orders-route.js
+++ routes/orders-route.js
@@ -3,8 +3,10 @@ const router = express.Router();
 const ordersController = require("../controllers/orders-controller");
 const validateLogin = require("../middlewares/authenticate");
 const validateUser = require("../middlewares/active");
+const validateType = require("../middlewares/type");
 
 router.get("/", [validateLogin, validateUser], ordersController.getFromUser);
+router.get("/merchant", [validateLogin, validateUser, validateType.checkMerchant], ordersController.getFromMerchant);
 router.post("/create", [validateLogin, validateUser], ordersController.create);
 router.delete("/cancel", [validateLogin, validateUser], ordersController.cancel);
 
