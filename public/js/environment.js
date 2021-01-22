7ee6357d53eb81c28271a94860669c44a8c0aa48
Luís Pereira <67525203+luispereira1999@users.noreply.github.com>
2021-01-22 02:16:13 +0000

    Corrigir url's

--- public/js/environment.js
+++ public/js/environment.js
@@ -1,8 +1,8 @@
 let currentOrigin = window.location.origin;
 
 if (currentOrigin == "file://")
-   var urlApi = "http://localhost:4000/api/";
-else if (currentOrigin == "http://localhost:4000")
-   var urlApi = "http://localhost:4000/api/";
+   var urlApi = "http://localhost:8080/api/";
+else if (currentOrigin == "http://localhost:8080")
+   var urlApi = "http://localhost:8080/api/";
 else
-   var urlApi = "https://picandgo.azurewebsites.net/api/";
+   var urlApi = "http://picandgo.westeurope.cloudapp.azure.com/api/";
